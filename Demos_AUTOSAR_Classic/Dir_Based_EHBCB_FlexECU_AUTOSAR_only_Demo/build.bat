60a57d7c64a941888651af9b4d43daeeb12b13ff
Frey Patrick (ETAS-DEV/XPC-Fe1) <patrick.frey@etas.com>
2022-11-24 09:11:11 +0100

    misc: Updated build.bat file for demo Dir_Based_EHBCB_FlexECU_AUTOSAR_only_Demo to use eHandbook.exe defined via environment viariable defined in set_tool_paths.bat

--- Demos_AUTOSAR_Classic/Dir_Based_EHBCB_FlexECU_AUTOSAR_only_Demo/build.bat
+++ Demos_AUTOSAR_Classic/Dir_Based_EHBCB_FlexECU_AUTOSAR_only_Demo/build.bat
@@ -5,12 +5,12 @@ SET EHB_CB_INPUT_DIR="%ARXML_DIR%_ehb_cb_input"
 
 md %EHB_CB_INPUT_DIR%
 
-C:\ETAS\EHANDBOOK-Container-Build_10.1.0-Windows\eHandbookCB.exe ^
+%EHB_CB_PATH%\eHandbookCB.exe ^
 -i %ARXML_DIR% ^
 -o %EHB_CB_INPUT_DIR% ^
 -ar2ehbcbinput
 
-C:\ETAS\EHANDBOOK-Container-Build_10.1.0-Windows\eHandbookCB.exe ^
+%EHB_CB_PATH%\eHandbookCB.exe ^
 -i %EHB_CB_INPUT_DIR% ^
 -n %ARXML_DIR% ^
 -o "./Output"
