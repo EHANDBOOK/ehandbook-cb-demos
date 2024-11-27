65367e28a398a7c44d05c65977cb3422ad2df3e0
Sagar Nayak(MS/EDV2-ETAS) <Nayak.Sagar@in.bosch.com>
2024-11-27 12:19:47 +0530

    EHBCB-9440: Update CB Demos for signal mapping feature.

---
+++ Demo_AUTOSAR_Signal_Mapping/build.bat
@@ -0,0 +1,11 @@
+cls
+
+C:\00_work\00_installer\EHB\EHB_CB_V11.3\eHandbookCB.exe ^
+-i ".\Input" ^
+-o ".\Output" ^
+-n test_mapping_signals ^
+-gensvg ^
+-nomatlab ^
+-matlabpath "C:\Program Files\MATLAB\R2019b\bin" ^
+-simlib "C:\Program Files\MATLAB\R2019b\toolbox\simulink" ^
+-componentconnectormappingfile ".\ComponentMapping.json"
