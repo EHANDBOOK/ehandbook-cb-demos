4013a092af70099377c94628523dc52be146548d
Patrick Frey (ETAS-ECM/XPC-Fe6) <patrick.frey@etas.com>
2025-12-12 19:23:32 +0100

    Updated set_tool_paths.bat for EHB 13.5.0

--- set_tool_paths.bat
+++ set_tool_paths.bat
@@ -1,3 +1,3 @@
-set EHB_CB_PATH=C:\ETAS\EHANDBOOK-Container-Build_11.0.0_Windows
-set EHB_UGG_PATH=C:\ETAS\EHANDBOOK-UGG_11.0.0_Windows
-set MATLAB_PATH=C:\Program Files\MATLAB\R2021a
+set EHB_CB_PATH=C:\ETAS\EHANDBOOK-Container-Build_13.5.0_Windows
+set EHB_UGG_PATH=C:\ETAS\EHANDBOOK-Container-Build_13.5.0_Windows
+set MATLAB_PATH=C:\Program Files\MATLAB\R2021a 
