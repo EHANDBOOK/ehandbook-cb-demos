f82a3220c2c664cb121a0ee8fc3e147918b24fd3
Patrick Frey (ETAS-ECM/XPC-Fe6) <patrick.frey@etas.com>
2025-12-13 14:56:17 +0100

    Fixed ComponentMapping.json in Demo_EHBCB_DirBased_AUTOSAR_Signal_Mapping

--- Demo_EHBCB_DirBased_AUTOSAR_Signal_Mapping/build.bat
+++ Demo_EHBCB_DirBased_AUTOSAR_Signal_Mapping/build.bat
@@ -6,6 +6,5 @@ cls
 -n "Demo_EHBCB_DirBased_AUTOSAR_Signal_Mapping" ^
 -gensvg ^
 -nomatlab ^
--matlabpath "%MATLAB_PATH%\bin" ^
 -simlib "%MATLAB_PATH%\toolbox\simulink" ^
 -componentconnectormappingfile ".\ComponentMapping.json"
