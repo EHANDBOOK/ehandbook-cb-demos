72f4c79b3b24e17a9b8351acd1e6cbb395ea5ad3
Johannes Ziegler <johannes.ziegler@etas.com>
2026-03-17 13:54:23 +0100

    misc: Fix review comments

--- Demo_EHBCB_DirBased_CCODE_CustomBlockConfiguration/CCodeLib/lib.c
+++ Demo_EHBCB_DirBased_CCODE_CustomBlockConfiguration/CCodeLib/lib.c
@@ -1,7 +1,7 @@
 int mul(int factor1, int factor2) {
-	return factor1 * factor2;
+    return factor1 * factor2;
 }
 
 int div(int dividend, int divisor) {
-	return dividend / divisor;
+    return dividend / divisor;
 }
